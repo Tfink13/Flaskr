93a7f080d0a722569035fd0dc9ce2127b21035b0
Tyler Fink <tsfink182@stevenscollege.edu>
2019-02-28 01:01:51 -0500

    Created registration form for user to create an account  - validating username and password are not empty  - hashing passwords  - redirecting to login once successful

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,3 +10,43 @@ from flaskr.db import get_db
 # __name__ is passed as the second arg b/c it has to know where it is being defined
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 # URL prefix is prepended to all other associated URL's
+
+
+# Once the user visits /auth/register they will have an HTML form
+# to fill out. Once submitted it will vallidate input and either
+# error, or create the new user
+
+# @bp.route associates the URL /register with the register view function
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    # validating the request from form submission
+    if request.method == 'POST':
+        # request.form is a special dict mapping
+        # users put in the info into username and password field
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+        # Once validation is successful the password is stored with a hash
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            #  required after querys are modifying the data
+            db.commit()
+            # After user is stored they are redirected to the login page
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
