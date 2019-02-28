3c0f51b54f6611e4048baddfc7c200188f8f6141
Tyler Fink <tsfink182@stevenscollege.edu>
2019-02-28 00:17:17 -0500

    Python functions to run the SQL

--- flaskr/db.py
+++ flaskr/db.py
@@ -23,3 +23,18 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+# functions thta will be running the SQL commands within the schema.sql file
+def init_db():
+    db = get_db()
+    # open_resource() opens a file relative to the flaskr package
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+# click.command() difines a command line command; this case init_db() to print a success login 
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
