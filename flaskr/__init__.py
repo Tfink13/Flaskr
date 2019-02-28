2fc75fa2427f954626a413f9d90cce30d4a27d5b
Tyler Fink <tsfink182@stevenscollege.edu>
2019-02-28 00:04:51 -0500

    Cleaned up the __init__.py file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,12 @@
 import os
 
 from flask import Flask
-# application factory ***
-def create_app(test_config=none):
-    # create and config application; flask instance 
+
+# # application factory ***
+def create_app(test_config=None):
+    # create and config application; flask instance
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mappping(
+    app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
@@ -15,16 +16,17 @@ def create_app(test_config=none):
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed
-        app.config.from_mappping(test_config)
+        app.config.from_mapping(test_config)
 
-    # ensure instance folder exists
+     # ensure instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    # page says hello
+
+    # page says hello world!
     @app.route('/hello')
     def hello():
-        return 'Hello World!'
+        return 'Hello, World!'
 
     return app
